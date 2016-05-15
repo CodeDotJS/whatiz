faf4a3a5e5022870c1f10622c4a8cda364574970
CodeDotJS <rushforlinux@gmail.com>
2016-05-15 06:06:31 +0530

    err removed

---
+++ cli.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const user = require('./index.js');
+
+user('express').then(shit => {
+	console.log(shit);
+});
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 'use strict';
 
-var cheerio = require('cheerio');
-
 var got = require('got');
+
+var cheerio = require('cheerio');
 
 var Promise = require('pinkie-promise');
 
 module.exports = function (packageName) {
 	if (typeof packageName !== 'string') {
-		return Promise.reject(new Error('package\'s name required'));
+		return Promise.reject(new Error('package name required'));
 	}
 
-	var url = 'https://npmjs.com/package/' + packageName;
+	var url = 'https://www.npmjs.com/packages/' + packageName;
 
 	return got(url).then(function (res) {
 		var $ = cheerio.load(res.body);
@@ -19,7 +19,7 @@ module.exports = function (packageName) {
 		return $('.package-description').text() || null;
 	}).catch(function (err) {
 		if (err.statusCode === 404) {
-			err.message = 'Package not available';
+			err.message = 'Package doesn\'t exist';
 		}
 
 		throw err;
